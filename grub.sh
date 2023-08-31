fce41baffeed65637bad00b336a0ec141e1651ac
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-08-31 18:16:53 +0200

    Create grub.sh

---
+++ grub.sh
@@ -0,0 +1,35 @@
+#/bin/bash
+echo "    $magenta -- $re Installing Tard-Grub 2.0 $magenta -- $re
+" sleep 1 ;
+mkdir -p -m 775 /boot/grub/ ; 
+cd /boot/grub/ ;
+# wget https://github.com/12ants/ss/raw/main/tard.jpg ;
+wget -O tard.jpg https://github.com/12ants/00/raw/main/img/tard2.jpg;
+rm /etc/default/grub.d/*;
+echo '
+GRUB_BACKGROUND="/boot/grub/tard.jpg"
+GRUB_DEFAULT=saved
+GRUB_SAVEDEFAULT=true
+GRUB_TIMEOUT_STYLE=menu
+GRUB_TIMEOUT=6
+GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
+GRUB_DISABLE_OS_PROBER=false
+GRUB_HIDDEN_TIMEOUT=0
+GRUB_CMDLINE_LINUX="" 
+GRUB_CMDLINE_LINUX_DEFAULT="--" 
+GRUB_TERMINAL_OUTPUT="console"
+GRUB_CMDLINE_LINUX="/root"
+GRUB_DISABLE_RECOVERY="false"
+
+
+' > /etc/default/grub; 
+
+update-grub ; 
+echo -e "\v\t -- GRUB INSTALLED SUCCESSFULLY \v"
+echo "  ####################################
+  #### -All done!--###################
+  ####################################
+
+
+
+"

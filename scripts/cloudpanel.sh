a6f5e16ce47aa1dcbe63af00f3317258d2ed4dde
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-09-24 00:09:58 +0200

    Create cloudpanel.sh

---
+++ scripts/cloudpanel.sh
@@ -0,0 +1,5 @@
+
+apt -y install curl ssh 
+curl -sS https://installer.cloudpanel.io/ce/v2/install.sh -o install.sh; \
+echo "3c30168958264ced81ca9b58dbc55b4d28585d9066b9da085f2b130ae91c50f6 install.sh" | \
+sha256sum -c && sudo DB_ENGINE=MARIADB_10.11 bash install.sh
